c1542ed540ffbad574c5ce5e571af2b3bed1b7ad
BasDM <bas.demeurichy@gmail.com>
2026-01-29 23:26:47 +0100

    Apply background to detail page

--- src/app/routes/detail.$id.tsx
+++ src/app/routes/detail.$id.tsx
@@ -2,12 +2,24 @@ import { Button, Container } from "@mui/material";
 import { useNavigate } from "react-router";
 import { useParams } from "react-router";
 
+import backgroundImage from "../../assets/brick_wall.png";
+
 export default function OverviewDetailRoute() {
     const { id } = useParams();
     const navigate = useNavigate();
 
     return (
-        <Container sx={{ py: 4 }}>
+        <Container 
+            maxWidth="md"
+            sx={{ 
+                py: 4,
+                minHeight: "100vh",
+                backgroundImage: `url(${backgroundImage})`,
+                backgroundRepeat: "repeat",
+                backgroundSize: "auto",
+                color: "white",
+            }}
+        >
             <Button 
                 onClick={() => navigate(-1)}
                 variant="contained"
