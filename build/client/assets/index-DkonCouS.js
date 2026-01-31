18084387869df4cc0e76a089245a96f2f9934acd
Ben De Meurichy <48103261+Bendemeurichy@users.noreply.github.com>
2026-01-31 23:26:40 +0100

    change build folder

---
+++ build/client/assets/index-DkonCouS.js
@@ -0,0 +1,1 @@
+import{p as s,a as e}from"./chunk-EPOLDU6W-DkCLH2QQ.js";const r=e.createContext();function n({value:t,...o}){return s.jsx(r.Provider,{value:t??!0,...o})}const u=()=>e.useContext(r)??!1;export{n as R,u};
